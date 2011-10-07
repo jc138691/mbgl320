25c15f012873f6decae086020ee46eb224cf22a1
jc138691 <jc138691@4e06dbda-22ce-5f7a-bdbb-d13d17f418f6>
2011-10-07 02:18:39 +0000

    Failed commit: Default (2)

--- trunk/java/lapack4j/lapack4j/Dbl2D.java
+++ trunk/java/lapack4j/lapack4j/Dbl2D.java
@@ -2,9 +2,7 @@ package lapack4j;
 /**
  * dmitry.a.konovalov@gmail.com,dmitry.konovalov@jcu.edu.com,5/10/11,1:35 PM
  */
-public class Dbl2D {
-  private final static int DUFF_SIZE = 8; // see p141 of C++, 3rd ed.
-
+public class Dbl2D extends Lapack4J {
   public static String toString(double[][] arr, int nRows, int nCols) {
     StringBuffer buff = new StringBuffer();
     buff.append(arr.toString() + " = " + TestUtils.EOL);
@@ -16,32 +14,56 @@ public class Dbl2D {
     return buff.toString();
   }
 
-  public static void copyFast(double[][] from, double[][] to, int nRows, int nCols) {
-    if (from == to) {
+  public static void copyFast(double[][] src, double[][] dest, int nRows, int nCols) {
+    if (src == dest) {
       return;
     }
     int n = (nRows + DUFF_SIZE - 1) / DUFF_SIZE - 1;
     int i = 0;
     switch (nRows % DUFF_SIZE) {
-      case 0:  System.arraycopy(from[i], 0, to[i], 0, nCols);   i++;
-      case 7:  System.arraycopy(from[i], 0, to[i], 0, nCols);   i++;
-      case 6:  System.arraycopy(from[i], 0, to[i], 0, nCols);   i++;
-      case 5:  System.arraycopy(from[i], 0, to[i], 0, nCols);   i++;
-      case 4:  System.arraycopy(from[i], 0, to[i], 0, nCols);   i++;
-      case 3:  System.arraycopy(from[i], 0, to[i], 0, nCols);   i++;
-      case 2:  System.arraycopy(from[i], 0, to[i], 0, nCols);   i++;
-      case 1:  System.arraycopy(from[i], 0, to[i], 0, nCols);   i++;
+      case 0:  System.arraycopy(src[i], 0, dest[i], 0, nCols);   i++;
+      case 7:  System.arraycopy(src[i], 0, dest[i], 0, nCols);   i++;
+      case 6:  System.arraycopy(src[i], 0, dest[i], 0, nCols);   i++;
+      case 5:  System.arraycopy(src[i], 0, dest[i], 0, nCols);   i++;
+      case 4:  System.arraycopy(src[i], 0, dest[i], 0, nCols);   i++;
+      case 3:  System.arraycopy(src[i], 0, dest[i], 0, nCols);   i++;
+      case 2:  System.arraycopy(src[i], 0, dest[i], 0, nCols);   i++;
+      case 1:  System.arraycopy(src[i], 0, dest[i], 0, nCols);   i++;
     }
     for (; n > 0; n--) {
-      System.arraycopy(from[i], 0, to[i], 0, nCols);   i++;
-      System.arraycopy(from[i], 0, to[i], 0, nCols);   i++;
-      System.arraycopy(from[i], 0, to[i], 0, nCols);   i++;
-      System.arraycopy(from[i], 0, to[i], 0, nCols);   i++;
-      System.arraycopy(from[i], 0, to[i], 0, nCols);   i++;
-      System.arraycopy(from[i], 0, to[i], 0, nCols);   i++;
-      System.arraycopy(from[i], 0, to[i], 0, nCols);   i++;
-      System.arraycopy(from[i], 0, to[i], 0, nCols);   i++;
+      System.arraycopy(src[i], 0, dest[i], 0, nCols);   i++;
+      System.arraycopy(src[i], 0, dest[i], 0, nCols);   i++;
+      System.arraycopy(src[i], 0, dest[i], 0, nCols);   i++;
+      System.arraycopy(src[i], 0, dest[i], 0, nCols);   i++;
+      System.arraycopy(src[i], 0, dest[i], 0, nCols);   i++;
+      System.arraycopy(src[i], 0, dest[i], 0, nCols);   i++;
+      System.arraycopy(src[i], 0, dest[i], 0, nCols);   i++;
+      System.arraycopy(src[i], 0, dest[i], 0, nCols);   i++;
     }
   }
 
+  public static void setCol(double[][] dest, int colIdx, double val, int len) {
+    int n = (len + DUFF_SIZE - 1) / DUFF_SIZE - 1;
+    int i = 0;
+    switch (len % DUFF_SIZE) {
+      case 0:  dest[i++][colIdx] = val;
+      case 7:  dest[i++][colIdx] = val;
+      case 6:  dest[i++][colIdx] = val;
+      case 5:  dest[i++][colIdx] = val;
+      case 4:  dest[i++][colIdx] = val;
+      case 3:  dest[i++][colIdx] = val;
+      case 2:  dest[i++][colIdx] = val;
+      case 1:  dest[i++][colIdx] = val;
+    }
+    for (; n > 0; n--) {
+      dest[i++][colIdx] = val;
+      dest[i++][colIdx] = val;
+      dest[i++][colIdx] = val;
+      dest[i++][colIdx] = val;
+      dest[i++][colIdx] = val;
+      dest[i++][colIdx] = val;
+      dest[i++][colIdx] = val;
+      dest[i++][colIdx] = val;
+    }
+  }
 }
