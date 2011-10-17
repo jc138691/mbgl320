dde5e25b7790f0a12b7e215e7bcc2819ddc3e6c5
jc138691 <jc138691@4e06dbda-22ce-5f7a-bdbb-d13d17f418f6>
2011-10-17 04:42:15 +0000

    Failed commit: Default (2)

--- trunk/java/lapack4j/lapack4j/lapack2j/Lapack2J.java
+++ trunk/java/lapack4j/lapack4j/lapack2j/Lapack2J.java
@@ -9,20 +9,23 @@ import java.util.ArrayList;
  * dmitry.a.konovalov@gmail.com,dmitry.konovalov@jcu.edu.com,13/10/11,11:55 AM
  */
 public class Lapack2J extends TestUtils {
-  public static final String SRC_DIR = "C:\\dev\\physics\\dev_svn_110812\\java\\lapack4j\\lapack4j\\fortran\\src";
-  public static final String DEST_DIR = "C:\\dev\\physics\\dev_svn_110812\\java\\lapack4j\\lapack4j\\lapack2j\\output";
+  public static String SRC_DIR = "C:\\dev\\physics\\dev_svn_110812\\java\\lapack4j\\lapack4j\\src\\fortran";
+  public static String DEST_DIR = "C:\\dev\\physics\\dev_svn_110812\\java\\lapack4j\\lapack4j\\lapack2j\\output";
+  public static String JOB_TAG = "_java.f";
   public static void main(String[] args) {
-    convert(SRC_DIR, "dstemr.f", DEST_DIR);
+    DEST_DIR = SRC_DIR;
+    new Lapack2J().convert(SRC_DIR, "lsame_src.f", DEST_DIR);
+    new Lapack2J().convert(SRC_DIR, "dstemr_src.f", DEST_DIR);
     System.exit(0);
   }
-  private static void convert(String srcDir, String fileName, String destDir) {
+  public  void convert(String srcDir, String fileName, String destDir) {
     String srcName = srcDir + File.separatorChar + fileName;
     File srcFile = new File(srcName);
     if (srcFile == null  || !srcFile.canRead()) {
       System.out.println("if (srcFile == null  || !srcFile.canRead()) { srcFile = new File(" + srcName);
     }
     String destName = destDir + File.separatorChar + fileName;
-    destName = FileUtils.replaceDotExtension(destName, "_java.f");
+    destName = FileUtils.replaceDotExtension(destName, JOB_TAG);
     File destFile = new File(destName);
     if (destFile == null  || !destFile.canWrite()) {
       System.out.println("if (destFile == null  || !destFile.canWrite()) { destFile = new File(" + destName);
@@ -32,32 +35,162 @@ public class Lapack2J extends TestUtils {
     ArrayList<String> res = convert2J(src);
     FileUtils.write(res, destFile);
   }
-  private static ArrayList<String> convert2J(ArrayList<String> src) {
+  public ArrayList<String> convert2J(ArrayList<String> src) {
     ArrayList<String> res = new ArrayList<String>();
     for (int i = 0; i < src.size(); i++) {
-      String line = src.get(i);
-      if (line == null)
+      String srcLine = src.get(i);
+      if (srcLine == null)
         break;
-      debug(line);
-      String lineTrim = line.trim();
-      if (line.isEmpty()) {
-        res.add(line);
-        continue;
-      }
-      if (Fortran77.comment(line)) {
-        res.add("//" + line);
-        continue;
-      }
-      if (Fortran77.subroutine(lineTrim)) {
-        String name = Fortran77.getSubroutine(lineTrim);
-        res.add("public class " + name);
-        continue;
-      }
-      if (Fortran77.endif(lineTrim)) {
-        res.add("} // " + lineTrim);
-        continue;
-      }
+      debug(srcLine);
+      if (isEmpty(srcLine, res))  continue;
+      if (comment(srcLine, res))  continue;
+
+      String srcTrim = srcLine.trim();
+      if (ignore(srcTrim, res))  continue;
+//      if (splitLine(line, res))  continue;
+      if (subroutine(srcTrim, res))  continue;
+      if (func(srcTrim, res))  continue;
+      if (type(srcTrim, res))  continue;
+      if (endif(srcTrim, res))  continue;
+      if (isReturn(srcTrim, res))  continue;
+      if (end(srcTrim, res))  continue;
+
+      String line = convertLine(srcLine);
+      res.add(line.trim() + "; //" + srcTrim);
+//      res.add("ERROR: TODO //" + srcTrim);
     }
     return res;
   }
+  public String convertType(String typeName) {
+     if (!Fortran77.type(typeName))
+       return "ERROR: !type(typeName)";
+
+    if (typeName.equals("CHARACTER"))
+      return "char";
+    if (typeName.equals("LOGICAL"))
+      return "boolean";
+    if (typeName.equals("INTEGER"))
+      return "int";
+    if (typeName.equals("DOUBLE PRECISION"))
+      return "double";
+    return "ERROR: convertType(String typeName)";
+  }
+
+  public String convertLine(String src) {
+    String res = src.replace("(*)", "[]");
+    res = res.replace("( * )", "[]");
+    res = res.replace("( *)", "[]");
+    res = res.replace("(* )", "[]");
+    res = res.replace(".EQ.", " == ");
+    res = res.replace(".GE.", " >= ");
+    res = res.replace(".GT.", " > ");
+    res = res.replace(".LE.", " <= ");
+    res = res.replace(".LT.", " < ");
+    res = res.replace(".AND.", " && ");
+    res = res.replace(".OR.", " || ");
+    return res;
+  }
+
+  public boolean isEmpty(String srcLine, ArrayList<String> dest) {
+    if (srcLine.isEmpty()) {
+      dest.add(srcLine);
+      return true;
+    }
+    return false;
+  }
+  public boolean comment(String srcLine, ArrayList<String> dest) {
+    if (Fortran77.comment(srcLine)) {
+      dest.add("//" + srcLine);
+      return true;
+    }
+    return false;
+  }
+  public boolean ignore(String lineTrim, ArrayList<String> dest) {
+    if (Fortran77.ignore(lineTrim)) {
+      dest.add("//" + lineTrim);
+      return true;
+    }
+    return false;
+  }
+  public boolean splitLine(String srcLine, ArrayList<String> dest) {
+    if (Fortran77.splitLine(srcLine)) {
+      String str = Fortran77.removeSplit(srcLine);
+      dest.add(str + " //" + srcLine.trim());
+      return true;
+    }
+    return false;
+  }
+
+  public boolean endif(String lineTrim, ArrayList<String> dest) {
+    if (!Fortran77.endif(lineTrim))
+      return false;
+    dest.add("} // " + lineTrim);
+    return true;
+  }
+  public boolean end(String lineTrim, ArrayList<String> dest) {
+    if (!Fortran77.end(lineTrim))
+      return false;
+    dest.add("} // " + lineTrim);
+    return true;
+  }
+  public boolean isReturn(String lineTrim, ArrayList<String> dest) {
+    if (!Fortran77.isReturn(lineTrim))
+      return false;
+    dest.add("} // " + lineTrim);
+    return true;
+  }
+  public boolean type(String lineTrim, ArrayList<String> dest) {
+    if (!Fortran77.type(lineTrim))
+      return false;
+    String typeName = Fortran77.getTypeName(lineTrim);
+    String name = convertType(typeName);
+    String vars = Fortran77.getTypeVars(lineTrim);
+    vars = convertLine(vars);
+    dest.add(name + " " + vars + ";");
+//    dest.add(name + " " + vars + "; //" + lineTrim);
+    return true;
+  }
+  public boolean subroutine(String lineTrim, ArrayList<String> dest) {
+    if (!Fortran77.subr(lineTrim))
+      return false;
+    String name = Fortran77.getSubrName(lineTrim);
+    String params = Fortran77.getSubrWithParms(lineTrim);
+//    dest.add("public class " + name + " { ");
+//    dest.add("public static void " + params + " { ");
+    dest.add("public class " + name + " { //" + lineTrim);
+    dest.add("public static void " + params + " { //" + lineTrim);
+    return true;
+  }
+  public boolean func(String lineTrim, ArrayList<String> dest) {
+    if (!Fortran77.func(lineTrim))
+      return false;
+    String typeName = Fortran77.getTypeName(lineTrim);
+    String name = Fortran77.getFuncName(lineTrim);
+    String params = Fortran77.getFuncWithParms(lineTrim);
+//    dest.add("public class " + name + " { ");
+//    dest.add("public static void " + params + " { ");
+    dest.add("public class " + name + " { //" + lineTrim);
+    String javaType = convertType(typeName);
+    dest.add("public static " + javaType + params + " { //" + lineTrim);
+    return true;
+  }
+//  public boolean subr(String lineTrim, int i, ArrayList<String> src, ArrayList<String> dest) {
+//    if (!Fortran77.subr(lineTrim))
+//      return false;
+//    String name = Fortran77.getSubrName(lineTrim);
+//    String params = Fortran77.getSubrWithParms(lineTrim);
+//    dest.add("public class " + name + " { //" + lineTrim);
+//    dest.add("public static void " + params + "//" + lineTrim);
+//    for (int nextIdx = i+1; nextIdx < src.size(); nextIdx++) {
+//      String nextLine = src.get(nextIdx);
+//      if (nextLine == null)
+//        break;
+//      debug(nextLine);
+//      if (!splitLine(nextLine, dest))
+//        break;
+//    }
+//    dest.add("{");
+//    return true;
+//  }
+
 }
